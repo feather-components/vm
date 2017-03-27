d59be973b90b5f0977cbf8be2c4b35ff00e3458e
zhanghao639 <zhanghao639@pingan.com.cn>
2017-03-27 12:16:01 +0800

    commit

--- alert.vue
+++ alert.vue
@@ -1,34 +1,123 @@
-<template>
-<Alert>
-    <template slot="content">{{content}}</template>
-    <template slot="extras"></template>
-</Alert>
-</template>
+<style>
+.vmui-alert-content{
+    color: #3B4263;
+    font-size: 16px;
+    letter-spacing: 0;
+    line-height: 28px;
+    text-align: center;
+}
+
+.vmui-alert-extras{
+    margin-top: 8px;
+    margin-bottom: 16px;
+    color: #555;
+    font-size: 12px;
+    line-height: 20px;
+    text-align: center;
+}
+
+.vmui-alert-btn{
+    width: 45%;
+    height: 32px;
+    line-height: 32px;
+    display: inline-block;
+    border: 1px solid #878787;
+    border-radius: 100px;
+    font-size: 14px;
+    color: #555555;
+    margin: 0px 4px 4px 4px;
+}
+
+.vmui-alert-c-btn{
+    background: #F96854;
+    color: #fff;
+    border: 1px solid #F96854;
+}
+
+.vmui-alert-s-btn{
+    width: 90%;
+}
+</style>
 
 <script>
 import Vue from 'vue';
+import Modal from './widgets/modal';
 
-var Alert = module.exports = function(content, options, callback, manualClose){
-    options = options || {};
+var override = (callback) => {
+    return (...args) => {
+        if(typeof args[1] != 'string'){
+            args.splice(1, 0, '');
+        }else if(!args[1]){
+            args[1] = '';
+        }
 
+        return callback.apply(window, args);
+    }
+};
+
+var instance = (content, extras, buttons) => {
     var element = document.createElement('div');
-    element.innerHTML = Alert.template;
+    document.body.appendChild(element);
 
-    var instance = new Vue({
-        el: element,
+    return new Vue({
+        template: Alert.template,
         components: {
-            Alert: require('./widgets/alert')
+            Modal
         },
         data: {
             content: content,
-            buttons: options.buttons,
-            extras: options.extras
+            extras: extras,
+            buttons: buttons
+        },
+        methods: {
+            callButton: function(index){
+                this.buttons[index].callback.call(this.$children[0]);
+            }
+        }
+    }).$mount(element).$children[0];
+};
+
+var Alert = module.exports = override((content, extras, callback, manualClose) => {
+    return instance(content, extras, {
+        '确定': {
+            className: 'vmui-alert-btn vmui-alert-s-btn',
+            callback: function(){
+                callback && callback();
+                !manualClose && this.close();
+            }
         }
     });
+});
 
-    console.log(instance, instance.$el);
+Alert.confirm = override((content, extras, callback, manualClose) => {
+    return instance(content, extras, {
+        '取消': {
+            className: 'vmui-alert-btn',
+            callback: function(){
+                this.close();
+            }
+        },
 
-    document.body.appendChild(instance.$el);
-    return instance;
-};
+        '确定': {
+            className: 'vmui-alert-btn vmui-alert-c-btn',
+            callback: function(){
+                callback && callback();
+                !manualClose && this.close();
+            }
+        }
+    })
+});
+
 </script>
+
+<template>
+<Modal :visible="true">
+    <slot>
+        <div class="vmui-alert-content">{{content}}</div>
+        <div class="vmui-alert-extras" v-if="!!extras">{{extras}}</div>
+    </slot>
+    <template slot="footer">
+        <a href="javascript:" v-for="(props, index) in buttons" @click="callButton(index)" :class="props.className">{{index}}</a>
+    </template>
+</Modal>
+</template>
--- widgets/modal.vue
+++ widgets/modal.vue
@@ -1,44 +1,30 @@
 <template>
 <Shade :visible="visibility">
     <Overlay :visible="true" :class="className" position="center">
-        <section class="vmui-alert-content">
+        <section class="vmui-modal-header">
+            <slot name="header"></slot>
+        </section>
+        
+        <section class="vmui-modal-content">
             <slot></slot>
-            <section class="vmui-alert-extras">
-                <slot name="extras"></slot>
-            </section>
         </section>
        
-        <section class="vmui-alert-buttons">
-            <slot name="buttons"></slot>
+        <section class="vmui-modal-footer">
+            <slot name="footer"></slot>
         </section>
     </Overlay>
 </Shade>
 </template>
 
 <style>
-.vmui-alert{
+.vmui-modal{
     border-radius: 4px;
     width: 60%;
     padding: 16px 10px;
 }
 
-.vmui-alert-content{
-    color: #3B4263;
-    font-size: 16px;
-    letter-spacing: 0;
-    line-height: 28px;
-    text-align: center;
-}
-
-.vmui-alert-extras{
-    margin-top: 10px;
-    color: #555;
-    font-size: 12px;
-    line-height: 20px;
-}
-
-.vmui-alert-buttons{
-    margin-top: 10px;
+.vmui-modal-footer{
+    margin-top: 14px;
     text-align: center;
 }
 </style>
@@ -66,7 +52,7 @@ export default{
 
     computed: {
         className(){
-            return ('vmui-alert ' + (this.class || '')).trim()
+            return ('vmui-modal ' + (this.class || '')).trim()
         }
     }
 }
