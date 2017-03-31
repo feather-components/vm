24388095a7a7f7dd95d1a5a7823e1b0d5c966542
zhanghao <jsyczhanghao@sina.com>
2017-03-31 21:30:51 +0800

    commit

---
+++ searchbar/index.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <input type="text" :placeholder="placeholder" @input="input()" ref="kw" />
+</div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default{
+    props: {
+        placeholder: {
+            type: String,
+            default: "请输入关键字进行搜索"
+        }
+    },
+
+    data(){
+        return {
+            value: ''
+        };
+    },
+
+    methods: {
+        input(){
+            this.value = this.$refs.kw.value;
+            this.$emit('input', this.value);
+        }
+    }
+}
+</script>
