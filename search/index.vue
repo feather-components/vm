b225438e4b7a22081caa20a2a5499a53724f532f
zhanghao639 <zhanghao639@pingan.com.cn>
2017-04-01 13:32:45 +0800

    commit

--- search/index.vue
+++ search/index.vue
@@ -1,70 +1,33 @@
 <template>
-<form class="vmui-search" @submit.false="submit()">
-    <div class="vmui-search-input">
-        <i class="vmui-search-icon"></i>
-        <input type="text" :placeholder="placeholder" @input="input()" ref="kw" v-model="value" />
-        <a href="javascript:" class="vmui-search-clear" @touchstart="clear()" v-show="clearVisible">&times;</a>
-    </div>
-</form>
+<Page position="right" :fx="true" :visible="visibility" class="vmui-search-view">
+    <Topbar>
+        <Search></Search>
+        <a href="javascript:" class="vmui-search-view-cancel" @touchstart="close()">取消</a>
+    </Topbar>
+</Page>
 </template>
 
 <style>
-.vmui-search{
-    height: 2em;
-    padding: .5em 0px;
-    line-height: 2em;
+.vmui-search-view-cancel{
+    float: right;
+    display: inline-block;
 }
 
-.vmui-search-input{
-    height: 2em;
-    border-radius: 100px;
-    margin: 0px 1em;
-    background: rgba(255, 255, 255, 0.1);
-    overflow: hidden;
-
-    .vmui-search-icon{
-        background: url(./search_white@2x.png?__inline) center center no-repeat;
-        background-size: 100%;
-        margin: 1px .5em 0px .7em;
-        width: 1.25em;
-        float: left;
-        height: 2em;
-        display: inline-block;
-    }
-
-    input{
-        max-width: 75%;
-        height: 2em;
-        margin-top: 2px;
-        color: #fff;
-        float: left;
-        display: block;
-        border: 0px;
-        outline: none;
-        background: transparent;
-    }
-
-    .vmui-search-clear{
-        font-family: consolas;
-        text-decoration: none;
-        text-align: center;
-        color: #fff;
-        line-height: 1em;
-        width: 1em;
-        height: 1em;
-        display: inline-block;
-        margin: .5em .7em 0px 0px;
-        float: right;
-        border: 1px solid #fff;
-        border-radius: 100px;
-        transform: translateY(-1px);
-        -webkit-transform: translateY(-1px);
+.vmui-search-view{
+    .vmui-search-input{
+        margin-right: 30px;
     }
 }
 </style>
 
 <script>
+import Page from '../page';
+import Topbar from '../topbar';
+import Search from '../search';
+
 export default{
+    mixins: [Page],
+
     props: {
         placeholder: {
             type: String,
@@ -76,6 +39,12 @@ export default{
         return {
             value: ''
         };
+    },
+
+    components: {
+        Page,
+        Topbar,
+        Search
     },
 
     computed: {
