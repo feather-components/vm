7166157fb42b1d6cd2625294453564aa6d5a0339
zhanghao639 <zhanghao639@pingan.com.cn>
2017-04-07 23:49:55 +0800

    commit

--- filterbox/index.vue
+++ filterbox/index.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="vmui-filterbox" @click.stop="">
+<div :class="'vmui-filterbox' + (multiple ? ' vmui-filterbox-multiple' : '')" @click.stop="">
     <div v-for="(data, index) of filters" class="vmui-filterbox-column">
         <scroll :fill-height="false" ref="scroll" :style="filterStyle">
-            <a href="javascript:" v-for="(item, key) of data" v-html="itemFormatter(item)" @click="select(item, key, index)" :class="'vmui-filterbox-item' + (indexs[index] == data.value ? ' vmui-filterbox-selected' : '')"></a>
+            <a href="javascript:" v-for="(item, key) of data" v-html="itemFormatter(item)" @click="select(item, index)" :class="'vmui-filterbox-item' + (indexs[index] == item.value ? ' vmui-filterbox-ii' : '') + (isSelected(item, index) ? ' vmui-filterbox-selected' : '')"></a>
         </scroll>   
     </div>
 </div>
@@ -28,15 +28,36 @@
         text-decoration: none;
         display: inline-block;
         width: 100%;
+        box-sizing: border-box;
         font-size: .14rem;
         color: #555;
         line-height: .44rem;
         text-align: center;
         border-top: 1px solid #eee;
+        padding: 0px 15px;
+    }
+
+    .vmui-filterbox-ii{
+        color: #6281C2;
     }
 
     .vmui-filterbox-selected{
         color: #6281C2;
+
+        &:after{
+            content: "";
+            display: inline-block;
+            float: right;
+            height: .44rem;
+            width: .20rem;
+            background: url(./selected@2x.png) no-repeat center center;
+        }
+    }
+}
+
+.vmui-filterbox-multiple{
+    .vmui-filterbox-item{
+        text-align: left;
     }
 }
 </style>
@@ -131,7 +152,7 @@ export default{
 
     data(){
         return {
-            level: 0,
+            level: -1,
             filters: [],
             indexs: [],
             vals: []
@@ -181,15 +202,16 @@ export default{
 
             if(!source.length) return;
 
+            self.level++;
+
             if(self.level > 0){
                 source = source.map((item) => {
-                    item.__p__ = self.indexs[self.level];
+                    item.__p__ = self.indexs[self.level - 1];
                     return item;
                 });
             }
             
             self.filters = self.filters.slice(0, self.level).concat([source]);
-            console.log(self.indexs, self.filters);
             self.$nextTick(() => {
                 self.$refs.scroll[self.level].refresh();
             });
@@ -200,7 +222,7 @@ export default{
             var params = {}, level = self.level;
 
             if(level > 0){
-                params[self._names[level]] = self.vals[level];
+                params[self._names[level]] = self.indexs[level];
             }
 
             self.abort();
@@ -215,10 +237,13 @@ export default{
             this.$http && this.$http.abort();
         },
 
-        clear(level){
+        clear(level, clearFilters){
             var self = this;
 
-            self.filters = self.filters.slice(0, level + 1);
+            if(clearFilters){
+                self.filters = self.filters.slice(0, level + 1);
+            }
+            
             self.indexs = self.indexs.slice(0, level);
 
             if(!self.multiple){
@@ -233,33 +258,73 @@ export default{
 
             if(self.vals[level] == data.value) return;
 
-            self.clear(level);
+            self.clear(level, false);
+            self.indexs.push(data.value);
 
             if(!self.multiple){
                 self.vals[level] = data.value;
-            }else if(level == self._names.length - 1){
-                var index = self.vals[level].indexOf(data.value);
+            }else{
+                var index;
+  
+                if(typeof data.__p__ !== 'undefined'){
+                    var vals = self.vals[data.__p__] || [];
 
-                if(index > -1){
-                    self.vals[level].splice(index, 1);
+                    index = vals.indexOf(data.value);
+
+                    if(index > -1){
+                        vals.splice(index, 1);
+                    }else if(!self.multipleSize || vals.length < self.multipleSize){
+                        vals.push(data.value);
+                    }
+
+                    if(vals.length){
+                        self.vals[data.__p__] = vals;
+                    }else{
+                        delete self.vals[data.__p__];
+                    }
+                }else if(self._names.length == 1){
+                    index = self.vals.indexOf(data.value);
+
+                    if(index > -1){
+                        self.vals.splice(index, 1);
+                    }else if(vals.length < multipleSize){
+                        self.vals.push(data.value);
+                    }
                 }
             }
-            
-            self.$emit('select', self.vals);
 
-            self.level++;
+            self.$emit('select', self.vals);
+            console.log(self.vals);
 
             if(!self.isRemoteSource()){
                 if(data.children && data.children.length){
                     self.renderList(data.children);
                 }
-            }else if(self._names.length > self.level){
+            }else if(self._names.length - 1 > self.level){
                 self.renderFromRemote();
             }
         },
 
         isRemoteSource(){
             return typeof this.source == 'string';
+        },
+
+        isSelected(data, level){
+            var self = this;
+
+            if(self.multiple){
+                if(self._names.length == 2){
+                    if(level == 0){
+                        return data.value in self.vals;
+                    }else{
+                        return (self.vals[data.__p__] || []).indexOf(data.value) > -1;
+                    }
+                }else{
+                    return self.vals.indexOf(data.value) > -1;
+                }
+            }
+
+            return false;
         }
     }
 }
