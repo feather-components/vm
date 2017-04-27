b18aeda4271c1a82dd1a32e9de1779fe4e2657f4
潘英 <panying447@pinganfang.com>
2017-04-27 10:33:20 +0800

    add radio

---
+++ form/box.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <label class="lm-label">
+            <span class="lm-label-title" v-text="title"></span>
+            <slot name="sub"></slot>
+            <slot name="right"></slot>
+        </label>
+        <slot name=""></slot>
+    </div>
+</template>
+
+<style lang="css">
+import "../../legomini/build/legomini.css";
+</style>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
---
+++ form/radio.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <label class="lm-label">
+            <span class="lm-label-desc" v-text="title"></span>
+        </label>
+        <div class="lm-radio-plain" v-for="option in options">
+            <input type="radio" :disabled="option.disabled" :value="option.value" v-model="currentValue">
+            <label v-text="option.label"></label>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'v-radio',
+
+    props: {
+        title: {
+            type: String,
+            default: null
+        },
+
+        options: {
+          type: Array,
+          required: true
+        },
+
+        value: {
+            type: String,
+            default: ''
+        } 
+    },
+
+    data() {
+        return {
+            currentValue: this.value
+        };
+    },
+
+    watch: {
+        value(val) {
+            this.currentValue = val;
+        },
+
+        currentValue(val) {
+            this.$emit('input', val);
+        }
+    }
+}
+</script>
