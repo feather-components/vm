0a1b6f60015630a9f77fe410978e15a2bf5293a2
彭志亮 <EX-pengzhiliang001@pingan.com.cn>
2017-05-16 11:24:39 +0800

    tijiao

--- form/ranger.vue
+++ form/ranger.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="vmui-ranger">
-        <div v-if="labelShow">
-            <slot name="label-min">
-                <span class="label-min">{{range[0]}}</span>
-            </slot>
-            <slot name="label-max">
-                <span class="label-max">{{range[1]}}</span>
-            </slot>
+        <slot name="label-min">
+            <span class="label-min">{{range[0]}}</span>
+        </slot>
+        <slot name="label-max">
+            <span class="label-max">{{range[1]}}</span>
+        </slot>
+        <div v-if="valueShow">
             <slot name="label-value">
                 <div class="range-value" v-if="sliderNum==1">
                     {{val[1]}}
@@ -18,23 +18,15 @@
         </div>
         <div class="ranger">
             <div class="ranger-body" ref="ranger_body">
-                <div class="ranger-cover1" :class="{setting:setting}" :style="{
-                width:cover1$.width + 'px',
-                }" v-if="sliderNum==2"></div>
+                <div class="ranger-cover1" :class="{setting:setting}" ref="cover1"  v-if="sliderNum==2"></div>
                 <div class="ranger-connect-line"></div>
-                <div class="ranger-cover2" :class="{setting:setting}" :style="{
-                width:cover2$.width + 'px',
-                }"></div>
-                <div class='drag1' ref="drag1"
+                <div class="ranger-cover2" :class="{setting:setting}" ref="cover2" ></div>
+                <div class='drag1' ref="drag1" v-draggable="{axis: 'x'}"
                      @drag:start="onDragStart" @draging="onDraging" @drag:end="onDragEnd" :class="{setting:setting}"
-                     :style="{
-                     left:drag1$.left+'px',
-                     }" v-if="sliderNum==2"></div>
-                <div class='drag2' ref="drag2"
+                     v-if="sliderNum==2"></div>
+                <div class='drag2' ref="drag2" v-draggable="{axis: 'x'}"
                      @drag:start="onDragStart" @draging="onDraging" @drag:end="onDragEnd" :class="{setting:setting}"
-                     :style="{
-                     left:drag2$.left+'px',
-                     }"></div>
+                ></div>
             </div>
         </div>
 
@@ -108,7 +100,9 @@
         z-index: 1;
         background: #e1e1e1;
     }
-
+    .vmui-ranger .drag1{
+        z-index:3!important;
+    }
     .vmui-ranger .drag1, .vmui-ranger .drag2 {
         position: absolute;
         top: 0;
@@ -125,7 +119,8 @@
     }
 </style>
 <script>
-    import {Draggable} from '../draggable';
+    import channel from './channel';
+    import _ from '../helper';
     export default {
         props: {
             range: {
@@ -147,7 +142,7 @@
                 },
                 default: 1
             },
-            labelShow: {
+            valueShow: {
                 type: Boolean,
                 default: true
             }
@@ -155,34 +150,20 @@
         data () {
             return {
                 initLeft: 12, //drag块的left,常量
-                cover2$: {
-                    width: 0
-                },
-                cover1$: {
-                    width: 0
-                },
                 drag2$: {
-                    left: -12,
-                    touchStartLeft: -12
+                    touchStartLeft: 0
                 },
                 drag1$: {
-                    left: -12,
-                    touchStartLeft: -12
+                    touchStartLeft: 0
                 },
                 clientMaxWidth: 0,
-                setting: false
+                setting: false,
+                rangerNumber:0
             }
         },
         mounted () {
-            if (this.sliderNum === 2) {
-                new Draggable(this.$refs.drag1, {
-                    axis: 'x'
-                });
-            }
-            new Draggable(this.$refs.drag2, {
-                axis: 'x'
-            });
-            this.initRanger()
+            this.rangerNumber = this.range[1]-this.range[0]
+            this._initRanger()
         },
         watch: {
             val (val) {
@@ -191,48 +172,38 @@
         },
         methods: {
             /*判断value,是否正确*/
-            valueToCorrect () {
+            _valueToCorrect () {
+                this.val[1] =Math.max( this.val[0], this.val[1])
+                this.val[0] =Math.min( this.val[0], this.val[1])
                 this.val[0] = this.val[0] < this.range[0] ? this.range[0] : this.val[0]
                 this.val[1] = this.val[1] > this.range[1] ? this.range[1] : this.val[1]
             },
             /* 初始化处理 */
-            initRanger () {
+            _initRanger () {
                 let rbDom = this.$refs.ranger_body
                 this.clientMaxWidth = rbDom.offsetWidth
-                this.setValue()
+                this._setValue()
             },
-            setValue () {
-                this.valueToCorrect()
-                this.handleInitDrag2()
+            _setValue () {
+                this._valueToCorrect()
+                this._handleSetDrag2()
                 if (this.sliderNum === 2) {
-                    this.handleInitDrag1()
+                    this._handleSetDrag1()
                 }
             },
-            setInitDrag2 () {
-                this.drag2$.left = this.clientMaxWidth * ((this.val[1] - this.range[0]) / (this.range[1] - this.range[0])) - this.initLeft
+            _handleSetDrag2 () {
+                _.css(this.$refs.drag2, 'left', this.clientMaxWidth * ((this.val[1] - this.range[0]) / this.rangerNumber) - this.initLeft + 'px');
+                _.css(this.$refs.cover2, 'width', this.clientMaxWidth * (( this.range[1] - this.val[1]) / this.rangerNumber)+ 'px');
             },
-            setInitCover2 () {
-                this.cover2$.width = this.clientMaxWidth * (( this.range[1] - this.val[1]) / (this.range[1] - this.range[0]))
-            },
-            setInitDrag1 () {
-                this.drag1$.left = this.clientMaxWidth * ((this.val[0] - this.range[0]) / (this.range[1] - this.range[0])) - this.initLeft
-            },
-            setInitCover1 () {
-                this.cover1$.width = this.clientMaxWidth * (( this.val[0] - this.range[0]) / (this.range[1] - this.range[0]))
-            },
-            handleInitDrag2 () {
-                this.setInitDrag2()
-                this.setInitCover2()
-            },
-            handleInitDrag1 () {
-                this.setInitDrag1()
-                this.setInitCover1()
+            _handleSetDrag1 () {
+                var vr = this.val[0] - this.range[0]
+                _.css(this.$refs.drag1, 'left', this.clientMaxWidth * (vr / this.rangerNumber) - this.initLeft + 'px');
+                _.css(this.$refs.cover1, 'width', this.clientMaxWidth * (vr / this.rangerNumber) + 'px');
             },
             onDragStart (e){
-                console.log(e,111)
-                this.drag2$.touchStartLeft = this.drag2$.left
+                this.drag2$.touchStartLeft = this.$refs.drag2.offsetLeft
                 if (this.sliderNum === 2) {
-                    this.drag1$.touchStartLeft = this.drag1$.left
+                    this.drag1$.touchStartLeft = this.$refs.drag1.offsetLeft
                     this.$emit('drag:start', this.val, e)
                 } else {
                     this.$emit('drag:start', this.val[1], e)
@@ -240,54 +211,57 @@
             },
             /*拖动进行时*/
             onDraging (e) {
-                console.log(e,222)
                 if (e.target.className == 'drag1') {
-                    this.setDrag1(e)
-                    this.setCover1(e)
+                    this._setDrag1AndCover1(e)
                 } else {
-                    this.setDrag2(e)
-                    this.setCover2(e)
+                    this._setDrag2AndCover2(e)
                 }
-                this.setVal(e)
+                this._setVal(e)
             },
-            setCover1 (e) {
-                this.cover1$.width = this.drag1$.left + this.initLeft
-            },
-            setDrag1 (e) {
+            _setDrag1AndCover1 (e) {
                 let $drag1 = this.$refs.drag1
-                this.drag1$.left = this.drag1$.touchStartLeft + e.data.x
-                $drag1.style.transform = 'translate3d(0, 0, 0)'
-                if (this.drag1$.left >= this.drag2$.left) {
-                    this.drag1$.left = this.drag2$.left
-                } else if (this.drag1$.left < -this.initLeft) {
-                    this.drag1$.left = 0 - this.initLeft
-                }
+                _.css($drag1, 'left', this.drag1$.touchStartLeft + e.data.x + 'px');
+                _.css($drag1, 'transform', 'translate3d(0, 0, 0)');
+
+                $drag1.offsetLeft >= this.$refs.drag2.offsetLeft &&
+                _.css($drag1, 'left', this.$refs.drag2.offsetLeft + 'px')
+
+                $drag1.offsetLeft < -this.initLeft &&
+                _.css($drag1, 'left',  -this.initLeft + 'px')
+
+                $drag1.offsetLeft >= this.clientMaxWidth - 3 * this.initLeft && _.css($drag1, 'z-index', '10')
+                _.css(this.$refs.cover1, 'width', $drag1.offsetLeft + this.initLeft + 'px');
             },
-            setCover2 (e) {
-                this.cover2$.width = this.clientMaxWidth - (this.drag2$.left + this.initLeft)
-            },
-            setDrag2 (e) {
+            _setDrag2AndCover2 (e) {
                 let $drag2 = this.$refs.drag2
-                this.drag2$.left = this.drag2$.touchStartLeft + e.data.x
-                $drag2.style.transform = 'translate3d(0, 0, 0)'
-                if (this.drag2$.left > this.clientMaxWidth - this.initLeft) {
-                    this.drag2$.left = this.clientMaxWidth - this.initLeft
-                } else if (this.sliderNum === 2 && this.drag1$.left >= this.drag2$.left) {
-                    this.drag2$.left = this.drag1$.left
-                } else if (this.drag2$.left < -this.initLeft) {
-                    this.drag2$.left = 0 - this.initLeft
-                }
+                _.css($drag2, 'left', this.drag2$.touchStartLeft + e.data.x + 'px');
+                _.css($drag2, 'transform', 'translate3d(0, 0, 0)');
+
+                $drag2.offsetLeft > this.clientMaxWidth - this.initLeft &&
+                _.css($drag2, 'left', this.clientMaxWidth - this.initLeft + 'px')
+
+                this.sliderNum === 2 && this.$refs.drag1.offsetLeft >= $drag2.offsetLeft &&
+                _.css($drag2, 'left', this.$refs.drag1.offsetLeft + 'px')
+
+                $drag2.offsetLeft < -this.initLeft &&
+                _.css($drag2, 'left', -this.initLeft + 'px')
+
+                $drag2.offsetLeft <= this.initLeft && _.css($drag2, 'z-index', '10')
+                _.css(this.$refs.cover2, 'width', this.clientMaxWidth - ($drag2.offsetLeft + this.initLeft) + 'px');
             },
-            setVal (e) {
-                this.val[1] = (this.range[1] - this.range[0]) * ((this.clientMaxWidth - this.cover2$.width) / this.clientMaxWidth) + this.range[0]
+            _setVal (e) {
+                if(e.target.className == 'drag2'){
+                    this.val[1] = this.rangerNumber * ((this.clientMaxWidth - this.$refs.cover2.offsetWidth) / this.clientMaxWidth) + this.range[0]
+                } else {
+                    this.val[0] = this.$refs.cover1.offsetWidth / this.clientMaxWidth * this.rangerNumber + this.range[0]
+                }
                 if (this.sliderNum === 2) {
-                    this.val[0] = this.cover1$.width / this.clientMaxWidth * (this.range[1] - this.range[0]) + this.range[0]
                     this.$emit('updating', this.val, e)
                 } else {
                     this.$emit('updating', this.val[1], e)
                 }
+                this.$forceUpdate()
             },
-            /* 拖动进行时（结束）*/
             /*  拖动结束 */
             onDragEnd (e) {
                 if (this.sliderNum === 1) {
@@ -299,7 +273,7 @@
             /* 手动设置值 */
             jsToSetVal () {
                 let _$ = this
-                _$.setValue()
+                _$._setValue()
                 _$.setting = true
                 _$.$el.addEventListener('transitionend', (e) => {
                     _$.setting = false
