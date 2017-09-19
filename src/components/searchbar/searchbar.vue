75fe2504a70653e1c17154752b1fdc4b31501d7f
练庆楠 <LIANQINGNAN447@pingan.com.cn>
2017-09-19 13:49:28 +0800

    修复 type=search 无效的bug

--- src/components/searchbar/searchbar.vue
+++ src/components/searchbar/searchbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <form :class="'vm-searchbar vm-searchbar-' + theme" @submit.prevent="submit()">
+    <form :class="'vm-searchbar vm-searchbar-' + theme" @submit.prevent="submit()" action="javascript:;">
         <div class="vm-searchbar-inner">
             <i class="vm-searchbar-icon"></i>
             <input :type="searchButtonEnabled ? 'search': 'text'" :placeholder="placeholder" :maxlength="maxlength" @input.trim="input" :value="val" ref="input" @focus="$emit('focus')"  @click="$emit('click')" :readonly="readonly" />
