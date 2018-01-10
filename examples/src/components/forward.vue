b402a957a65d59e1efff497832e3b44208eae8ed
zhanghao <jsyczhanghao@sina.com>
2018-01-10 09:10:36 +0800

    commit

--- examples/src/components/forward.vue
+++ examples/src/components/forward.vue
@@ -2,8 +2,8 @@
     <vm-page style="background: #eee;">
         <vm-topbar slot="header">Forward 前进按钮</vm-topbar>
 
-        <div style="text-align: left; margin-left: 0.08rem;"><vm-forward style="height: 32px;">查看更多</vm-forward></div>
-       	<div style="text-align: right; margin-right: 0.08rem;"><vm-forward style="height: 32px;">查看更多</vm-forward></div>
+        <div><vm-forward style="height: 32px;">查看更多</vm-forward></div>
+       	<div>123<vm-forward style="height: 32px;">查看更多</vm-forward></div>
     </vm-page>
 </template>
 
--- src/components/forward/forward.vue
+++ src/components/forward/forward.vue
@@ -1,19 +1,27 @@
 <template>
-	<a href="javascript:" class="vm-forward">
-		<span class="vm-forward-content"><slot></slot></span>
-		<icon name="arrow" :size="0.12" />
-	</a>
+	<span :class="['vm-forward-wraper', leftLayout ? 'vm-forward-ll' : '']">
+		<a href="javascript:" class="vm-forward">
+			<span class="vm-forward-content"><slot></slot></span>
+			<icon name="arrow" :size="0.12" />
+		</a>
+	</span>
 </template>
 
 <style lang="less">
+	.vm-forward-wraper{
+		display: inline-flex;
+		flex: 1;
+		justify-content: flex-end;
+		align-items: center;
+		color: #222;
+		font-size: .14rem;
+	}
+
 	.vm-forward{
 		-webkit-tap-highlight-color: transparent;
 		text-decoration: none;
-		color: #222;
-		font-size: .14rem;
 		display: inline-flex;
-		align-items: center;
-		text-align: right;
+		color: inherit;
 
 		.vm-forward-content{
 			margin-right: .06rem;
@@ -21,9 +29,12 @@
 
 		.vm-iconfont{
 			font-weight: bold;
-			float: right;
         	transform: rotate(-90deg);
     	}
+	}
+
+	.vm-forward-ll{
+		justify-content: flex-start;
 	}
 </style>
 
@@ -41,6 +52,16 @@
 				type: Boolean,
 				default: null
 			}
+		},
+
+		data(){
+			return {
+				leftLayout: true
+			};
+		},
+
+		mounted(){
+			this.leftLayout = this.$el.parentNode.childNodes[0] === this.$el;
 		}
 	}
 </script>
