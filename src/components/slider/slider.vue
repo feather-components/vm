ca56223a844ce13e33e809258148861a71243646
zhanghao <jsyczhanghao@sina.com>
2018-05-11 09:42:42 +0800

    fixed bug

--- src/components/slider/slider.vue
+++ src/components/slider/slider.vue
@@ -90,7 +90,7 @@
             },
 
             to(index, transition = true, untrigger = false){
-                var left = this.$el.children[index].offsetLeft;
+                var left = -index * Dom.width(document);
 
                 this.transition = transition;
 
